dabc5f38c64f26650c7c1844fad2e05e0ff6c51f
Dorrington <36908162+MrSJDDevelopments@users.noreply.github.com>
2024-08-05 23:53:41 +0100

    re modification of cg.add.library

--- esphome/components/heatpumpir/climate.py
+++ esphome/components/heatpumpir/climate.py
@@ -128,6 +128,11 @@ def to_code(config):
     cg.add(var.set_max_temperature(config[CONF_MAX_TEMPERATURE]))
     cg.add(var.set_min_temperature(config[CONF_MIN_TEMPERATURE]))
 
-    cg.add_library("https://github.com/MrSJDDevelopments/heatpumpir.git#1.0.30")
+    cg.add_library(
+        None,
+        None,
+        "https://github.com/MrSJDDevelopments/heatpumpir.git#1.0.30",
+    )
+    #cg.add_library("https://github.com/MrSJDDevelopments/heatpumpir.git#1.0.30")
     if CORE.is_libretiny:
         CORE.add_platformio_option("lib_ignore", "IRremoteESP8266")
